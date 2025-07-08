538d16e9ae1baaae063be3e7c5849e9491c94cfe
ArunRajasekaran48 <arun.r2023cse@sece.ac.in>
2025-07-08 15:29:20 +0530

    Day 5 commit

---
+++ demo/src/main/java/com/training/demo/controllers/EmployeeController.java
@@ -0,0 +1,46 @@
+package com.training.demo.controllers;
+
+import com.training.demo.models.Employee;
+import com.training.demo.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/getEmployees")
+    public List<Employee> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+
+    @PostMapping("/createEmployee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+
+    @GetMapping("/getEmployeeById/{id}")
+    public Employee getEmployeeById(@PathVariable int id){
+        return employeeService.getEmployeeById(id);
+    }
+
+    @DeleteMapping("/deleteEmployee/{id}")
+    public String deleteEmployee(@PathVariable int id){
+        return employeeService.deleteEmployee(id);
+    }
+
+    @PutMapping("/updateEmployee/{id}")
+    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
+        Employee updated = employeeService.updateEmployee(id, employee);
+        if (updated != null) {
+            return updated;
+        } else {
+            throw new RuntimeException("Employee not found with id: " + id);
+        }
+    }
+}
