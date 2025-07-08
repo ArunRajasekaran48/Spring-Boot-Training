4a1f4585e4d6846aaf581dfbc45a47bf41cc6b39
ArunRajasekaran48 <arun.r2023cse@sece.ac.in>
2025-07-08 16:00:58 +0530

    Day 5 AN Commit

--- demo/src/main/java/com/training/demo/config/SpringConfiguration.java
+++ demo/src/main/java/com/training/demo/config/SpringConfiguration.java
@@ -3,10 +3,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){          //BCryptPasswordEncoder hashes passwords for secure storage.
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean//This class will define beans (objects managed by Spring).
     public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
         http.
@@ -14,5 +25,23 @@ public class SpringConfiguration {
                 .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetails(){
+        UserDetails admin= User.builder()//user1
+                .username("admin")
+                .password("admin")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails Rhythan= User.builder()//user2
+                .username("Arun")
+                .password(passwordEncoder().encode("Arun"))//encoding the password using above  passwordEncoder()
+                .roles("USER")
+                .build();
+        //return admin;
+        return new InMemoryUserDetailsManager(admin,Rhythan);
+
     }
 }
